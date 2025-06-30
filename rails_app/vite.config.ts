6c6929a3035475c1f97fa764756aac77309d2d3e
syuu2449 <syuu2449@gmail.com>
2025-07-01 01:06:40 +0900

    ・viteサーバー用コンテナを作る前に一旦コミット

--- rails_app/vite.config.ts
+++ rails_app/vite.config.ts
@@ -1,55 +1,20 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import vue from '@vitejs/plugin-vue'
-// import tailwindcss from "@tailwindcss/vite"; // v4用
-import fs from 'fs'
-import path from 'path'
-
-
-// 再帰的にエントリポイントを取得
-function getEntrypoints(dir) {
-  const entries = {}
-  const walk = (currentPath) => {
-    fs.readdirSync(currentPath).forEach(file => {
-      const fullPath = path.join(currentPath, file)
-      const stat = fs.statSync(fullPath)
-      if (stat.isDirectory()) {
-        walk(fullPath)
-      } else if (/\.(js|css|ts|tsx)$/.test(file)) {
-        const relativePath = path.relative(dir, fullPath).replace(/\\/g, '/')
-        entries[relativePath.replace(/\.(js|css)$/, '')] = fullPath
-      }
-    })
-  }
-  walk(dir)
-  return entries
-}
-
-
 
 export default defineConfig({
-  server: {
-    // host: '0.0.0.0',
-    host: true,
-    port: 3036,
-    strictPort: true,
-    // allowedHosts: ['rails_app', 'rails_nginx', 'localhost', 'localhost:3000'], // 許可するコンテナ名
-    // hmr: {
-    //   // host: 'nginx',
-    //   host: 'localhost',
-    //   protocol: 'ws',
-    //   port: 3036,
-    // },
-  },
-  // js,css,imagesで切り分ける場合自動でパスが入らない為、明示的に記載する
-  build: {
-    rollupOptions: {
-      input: getEntrypoints(path.resolve(__dirname, 'app/frontend'))
-    }
-  },
   plugins: [
     RubyPlugin(),
-    vue()
-    // tailwindcss() // v4用
+    vue(),
   ],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: 'localhost',
+      port: 5173,
+      protocol: 'ws'
+    }
+  }
 })
